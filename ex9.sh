abbd2d61bca604e57e9855114dbd9f06a5fd3a4c
kaioreidela <112896928+kaioreidela@users.noreply.github.com>
2022-09-26 15:02:40 -0300

    Update ex9.sh
    finalizada

--- ex9.sh
+++ ex9.sh
@@ -1,10 +1,62 @@
 
-echo "insira uma palavra a ser ordenada"
-res=""
-read palavra
-while [ $palavra != "sair" ]
-do 
- res= $res $palavra
- read palavra
+#!/bin/bash
+
+questao ()
+{
+read p
+echo "$p" | fmt -1 | sort
+}
+
+clear
+echo ""
+echo "Questão 9"
+echo ""
+
+echo "Desenvolva um script que receba uma lista de palavras como parâmetros e exiba, como resultado, essas palavras ordenadas."
+echo ""
+
+n1=0
+while [ $n1 -eq 0 ]
+do
+echo " essa e a questao que vc deseja [y/n]?"
+echo ""
+read opcao1
+clear
+   
+   if [ $opcao1 = "n" ]
+      then
+         n1=1
+   
+   elif [ $opcao1 = "y" ]
+      then
+         n1=2
+   else 
+      echo "                    erro" 
+   fi
 done
-echo "$res" | fmt -1 | sort
+
+while [ $n1 -eq 0 ] || [ $n1 -eq 2 ]
+do
+   if [ $n1 = 2 ]
+   then 
+   questao
+   fi  
+
+   echo " quer refazer a questao [y/n]?"
+   read opcao1
+   clear
+
+   if [ $opcao1 = "n" ]
+      then
+         n1=1
+   
+   elif [ $opcao1 = "y" ]
+      then
+         n1=2
+   else 
+      echo "                    erro" 
+         n1=0
+   fi 
+done
+
+
